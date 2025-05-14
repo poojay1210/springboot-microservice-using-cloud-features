e57ad544862712d68bc22f6e59793075e1c64404
pooja_yadav <pooja_yadav@epam.com>
2025-05-15 01:29:33 +0530

    file

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,17 +1,20 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @Value("${application.message:Not configured by Spring Cloud Config Server}")
-    private String message;
 
-    @GetMapping("/hello")
-    public String hello() {
-        return message + '\n';
+
+    @Value("${app.welcomeMsg}")
+    private String welcomeMsg;
+
+    @GetMapping("/welcome-msg")
+    public ResponseEntity<String> getWelcomeMessage(){
+        return ResponseEntity.ok(welcomeMsg);
     }
 }
